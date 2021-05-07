e7bfa66864854ca8678f3d8f4d4590cd9ea41f21
Lucas T Lopes <troleizlucas@gmail.com>
2021-05-07 10:46:17 -0300

    Adicionando metódo PUT

--- src/main/java/com/productms/productms/controller/productController.java
+++ src/main/java/com/productms/productms/controller/productController.java
@@ -5,11 +5,16 @@ import com.productms.productms.repository.productRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController("/products")
@@ -34,7 +39,7 @@ public class productController {
     }
 
     @GetMapping("/{id}")
-    public Object listOne(@PathVariable String id) {
+    public Object listProduct(@PathVariable String id) {
         if (productRepository.findById(id).isEmpty()) {
             logger.debug("Produto: " + id + " não existe");
             return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
@@ -42,6 +47,21 @@ public class productController {
             logger.debug("Listando produto id: " + id);
             return productRepository.findById(id);
         }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateUser(
+            @PathVariable(value = "id") String id, @RequestBody @Validated Product product) throws ResourceAccessException
+             {
+        Product product1 = productRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND,"Produto : "+ id + " não encontrado"));
+
+        product1.setName(product.getName());
+        product1.setDescription(product.getDescription());
+        product1.setPrice(product.getPrice());
+        final Product productUpdate = productRepository.save(product1);
+
+        return ResponseEntity.ok(productUpdate);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/productms/productms/entity/Product.java
+++ src/main/java/com/productms/productms/entity/Product.java
@@ -2,15 +2,24 @@ package com.productms.productms.entity;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 
 @Document(collection = "product")
 public class Product {
 
-        @Id
-        private String id;
-        private String name;
-        private String description;
-        private Double price;
+    @Id
+    private String id;
+
+    @NotNull(message = "Deve conter nome do produto")
+    private String name;
+
+    @NotNull(message = "Deve conter alguma breve descrição do produto")
+    private String description;
+
+    @NotNull(message = "Preço do produto deve ser algum valor positivo")
+    @PositiveOrZero
+    private Double price;
 
     public String getId() {
         return id;
@@ -28,4 +37,19 @@ public class Product {
         return price;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
